52adc3f9f33ebefbb8e4a9dc6bc04c19d98ac681
Samantha Rabling <sjarillorabling@gmail.com>
2018-05-07 15:19:57 -0700

    fixing bugs

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
+const server = app.listen(4000);
 const io = require("socket.io")(server);
-app.listen(4000);
+
 app.use(express.static(__dirname + "/public"));
 
 var counter = 0;
 
 io.on("connection", function(socket){
     socket.emit("button_counter", {counter: counter});
-    socket.on("button", function(data){
+    socket.on("pressed", function(data){
         counter++;
+        console.log(counter);
         io.socket.emit("button_counter", {counter: counter});
+        console.log(counter);
     });
     socket.on("reset", function(data){
         counter = 0;
